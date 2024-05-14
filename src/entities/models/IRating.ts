b7af86a294feedf2d10c426a0c48d4e22f769da6
DiioBrando <olsaf21f12@gmail.com>
2024-05-14 16:19:21 +0500

    удалил не использующийся компонент

--- src/entities/models/IRating.ts
+++
@@ -1,7 +0,0 @@
-import { ITypeRating } from '@/entities/data/anime-data/model/IAnimeArray';
-
-export interface IAnimeRating {
-  ratingName: string;
-  maxNumRating: number;
-  ratingObject: ITypeRating;
-}
