538157fd6eb247a9bcce0ab329fd74a3b3fae463
DiioBrando <olsaf21f12@gmail.com>
2024-06-07 14:37:39 +0500

    фикс запроса

--- src/features/anime/api/getListTitlesById.ts
+++ src/features/anime/api/getListTitlesById.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import AnimeService from '@/features/anime/lib/AnimeService';
 
-export const GetListTitlesById = ({ arrayId }: { arrayId: number[] }) => {
+export const GetListTitlesById = ({ arrayId }: { arrayId: string }) => {
   return useQuery({
     queryFn: async () => AnimeService.getAnimeListByArrayId(arrayId),
     queryKey: ['getListTitlesById', arrayId],
